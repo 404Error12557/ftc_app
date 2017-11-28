fb284842e87a3d4dc51f8a35594c7a1f6a54e2df
LAPTOP-PAB5HL9M\srajs <srajsen2@hotmail.com>
2017-11-27 22:04:21 -0800

    i updated the tele code with new comments.
    plz ask me if you have any questions i will put it on another update with more comment for veryone to learn more.:D

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp12557.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp12557.java
@@ -1,5 +1,3 @@
-
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -12,7 +10,6 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-
 import static android.os.SystemClock.sleep;
 
 /**
@@ -20,6 +17,7 @@ import static android.os.SystemClock.sleep;
  *
  */
 
+//This is a code for TeleOp mode - Driver control
 @TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
 //@Disabled
 public class TeleOp12557 extends OpMode
@@ -27,14 +25,13 @@ public class TeleOp12557 extends OpMode
     /*
     // Declare OpMode members.
     */
-
-    //Timer variable for displaying telemerty
+    //Timer variable for displaying telemerty/////////
     private ElapsedTime runtime = new ElapsedTime();
 
-    // Boolen variable for controling the speed of the Robot when left bumper switch is clicked.
+    // Boolen variable for controling the speed of the Robot when left bumper switch is clicked.///////////
     private boolean speedFlag = false;
 
-    // Instance variable for DcMotors. This includes motor for 4 drivetrain motors, two lift motors and one arm motor
+    // Instance variable for DcMotors. This includes motor for 4 drivetrain motors, two lift motors and one arm motor/////////
     private DcMotor motorRF = null; // Drivetrain Right Front motor
     private DcMotor motorRB = null; //Drivetrain Right Back motor
     private DcMotor motorLF = null; //Drivetrain Left Frount motor
@@ -43,21 +40,15 @@ public class TeleOp12557 extends OpMode
     private DcMotor motorLift1 = null; // Lift Motor 1
     private DcMotor motorArm = null; // Arm motor for tape measure
 
-    // Instance variable for Servos
+    // Instance variable for Servos////////////
     private Servo clawServo= null; // Claw Servo
-
-    private double linearPosition = 0.05;
-    static final double INCREMENT   = 0.01;
-
-
-
-    // amount to slew servo each CYCLE_MS cycle
 
 
     /*
      * Code to run ONCE when the driver hits INIT
      */
     @Override
+    //this will show up when init is clicked//////
     public void init() {
         telemetry.addData("Status", "Initialized");
 
@@ -65,18 +56,12 @@ public class TeleOp12557 extends OpMode
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
 
-
+        // Mapping Servos Name form the config file on the phone//////
         clawServo = hardwareMap.get(Servo.class, "claw_servo");
+        // we set a limitaion to how far the claw could widen because that would prevent it from over heating/////////
         clawServo.scaleRange(0.4,0.8);
 
-
-
-
-
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
-
-
+        // Mapping DcMotor name from the cofig files on the phone///////////
         motorLF  = hardwareMap.get(DcMotor.class, "mLF");
         motorRF = hardwareMap.get(DcMotor.class, "mRF");
         motorLB = hardwareMap.get(DcMotor.class, "mLB");
@@ -85,6 +70,7 @@ public class TeleOp12557 extends OpMode
         motorArm = hardwareMap.get(DcMotor.class, "mArm");
         motorLift1 =hardwareMap.get(DcMotor.class, "mLift1");
 
+        // Most robots need the motor on one side to be reversed to drive forward///////////
         motorLF.setDirection(DcMotor.Direction.REVERSE);
         motorRB.setDirection(DcMotor.Direction.REVERSE);
         motorArm.setDirection(DcMotor.Direction.REVERSE);
@@ -93,7 +79,7 @@ public class TeleOp12557 extends OpMode
         motorArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);//runs to use the encoder
 
 
-        // Tell the driver that initialization is complete.
+        // Tell the driver that initialization is complete./////////
         telemetry.addData("Status", "Initialized");
     }
 
@@ -101,6 +87,7 @@ public class TeleOp12557 extends OpMode
      * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
      */
     @Override
+    //what ever we want in here will run once then will not run another time/////////////
     public void init_loop() {
     }
 
@@ -115,11 +102,13 @@ q    @Override
      * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
      */
     @Override
+    // in here(the loop) what ever you want to happen while you are driving the robot will happen like movement///////////
     public void loop() {
 
         /*
         // Macnum wheel code.
         */
+        //we programmed the robot so that when we turn the left stick to the right it will strafe right////////////
         double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
         double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
         double rightX = gamepad1.right_stick_x;
@@ -128,7 +117,7 @@ q    @Override
         double v3 = r * Math.sin(robotAngle) + rightX;
         double v4 = r * Math.cos(robotAngle) - rightX;
 
-        //Code for controling the speed when left bumper switch is pressed
+        //Code for controling the speed when left bumper switch is pressed//////////////////
         if (gamepad1.left_bumper == true) {
             sleep(100);
             if (speedFlag == true)
@@ -164,7 +153,7 @@ q    @Override
         /*
         // Claw action
         */
-
+        // The claw sevo closes and opens the claw here we programmed it so when we click the right bumper it opens and when we let go it lets go/////////////////
         if(gamepad1.right_bumper == true) {
             clawServo.setPosition(1);
         }
@@ -174,7 +163,8 @@ q    @Override
 
 
         /*
-        //Lift action fsdlkfjdslfj
+        //Lift action when y is clicked on the controller then goes up when a is clicked it goes up/////////////
+        //adn we made it so when none is clicked we add a little power added because the base of teh claw and the linear slide was that heavy//////////////
         */
         if (gamepad1.y==true) {
 
@@ -193,6 +183,7 @@ q    @Override
         /*
         //Arm action
         */
+        //this allows it so the measuring tape is pushed fowrard so we can place the relic//////////////////
         if (gamepad1.dpad_up==true) {
             motorArm.setPower(1.0);
         }
@@ -210,6 +201,7 @@ q    @Override
 
         }
         */
+       //this is so that when nothing is preaaed then the arm will not move///////////////////
         if (gamepad1.dpad_up==false && gamepad1.dpad_down==false){
 
             motorArm.setPower(0);
@@ -233,12 +225,15 @@ q    @Override
         /*
         //Telemety
         */
-        // Show the elapsed game time and wheel power.
+        // Show the elapsed game time and wheel power.//////
         telemetry.addData("Name: ", "Sarvesh Robot");
         // telemetry.addData("Gamepad Status : ", "x (%.2f)", gamepad1.left_stick_x );
         //telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
         // telemetry.addData("Status", "Run Time: " + runtime.toString());
         // telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+
+        /*this is the telemtry so when the play button is clicked it will show us what motor is running and also how long have we been
+        running this programm and like how far is the arm extendend and all that cool stuff*//////////////////
         telemetry.addData("speedFlag", speedFlag);
         telemetry.addData("Gamepad1.start:",gamepad1.start);
         telemetry.addData("V1 :", v1);
@@ -257,6 +252,7 @@ q    @Override
     @Override
     public void stop() {
         /*
+        ////////this was supposed to be a code so when the stop button is pressed then the arm will be reeled back in////////////
         Do not remove this code
         motorArm.setTargetPosition(10);
         motorArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
